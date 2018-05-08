61e3035954890cbb1fc72d6d426bb41b9511822b
davin <davindavies@outlook.com>
2018-05-08 19:01:46 +0100

    Added GaneStats model and Interface IGame

---
+++ GoMemory/GoMemory/Interfaces/IGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+
+namespace GoMemory.Interfaces
+{
+    public interface IGame
+    {
+        void Initilize();
+
+        void Start();
+        void EndGame();
+
+        void CheckSelections();
+
+        void GenerateImages();
+    }
+}
---
+++ GoMemory/GoMemory/Models/GameStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoMemory.Models
+{
+   public class GameStats
+    {
+        public int Id { get; set; } 
+        public string GameTitle { get; set; }
+        public int BestLevel { get; set; }
+        public DateTime AverageTime { get; set; }
+
+    }
+}
